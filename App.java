ade5326401bb7a0d00742535b3a8901c934a8eca
Gathje <adarsh4arun@gmail.com>
2026-04-08 22:49:10 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
+        int n = 19;
 
         long factorial = 1;
 
